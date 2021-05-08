4561a5917929416f44b4e9a496a1871465289d7e
msj <359772486@qq.com>
2021-05-08 11:02:13 +0800

    Revert "2-9 集成热部署"
    This reverts commit f94966e0

--- src/main/java/com/my/wiki/controller/TestController.java
+++ src/main/java/com/my/wiki/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.my.wiki.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 //RestController 等于 Controller + ResponseBody(返回字符串或JSON对象)
 @RestController
 public class TestController {
-
-    @Value("${test.hello:TEST}")
-    private String testHello;
 
     /**
      * GET POST PUT DELETE
@@ -23,7 +19,7 @@ public class TestController {
 //    @RequestMapping(value = "/hello" , method = RequestMethod.GET)
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World" + testHello;
+        return "Hello World";
     }
 
     @PostMapping("/hello/post")
