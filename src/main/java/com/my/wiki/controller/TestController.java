cc5dd747644ba450acef1ae9f5f652c93b412b5c
msj <359772486@qq.com>
2021-05-08 10:24:26 +0800

    2-5 增加hello接口测试test.http

--- src/main/java/com/my/wiki/controller/TestController.java
+++ src/main/java/com/my/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.my.wiki.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //RestController和Controller的区别  一个返回字符串 一个返回页面
 //RestController 等于 Controller + ResponseBody(返回字符串或JSON对象)
@@ -18,6 +20,11 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World post" + name;
     }
 
 }
