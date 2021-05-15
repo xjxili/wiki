36118205c1fade76fd88c336e9a191b17ec618bd
msj <359772486@qq.com>
2021-05-15 15:30:40 +0800

    5-4 集成图标组件 npm install @ant-design/icons-vue@5.1.8 --save 导入到main.ts 加入到全局组件

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,5 +4,14 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css'
+//导入所有图标库
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app')
+
+//导入所有图标库
+const icons :any = Icons;
+for(const i in icons){
+    app.component(i,icons[i]);
+}
