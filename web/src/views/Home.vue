05f48c4b9134e91ad5ae1b61700ca1a1daaaf87d
msj <359772486@qq.com>
2021-05-15 15:19:28 +0800

    5-4 加入ant design vue list组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -47,10 +47,38 @@
         </a-menu>
       </a-layout-sider>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        <pre>
-{{ebooks}}
-{{ebooks2}}
-        </pre>
+        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+          <template #footer>
+            <div>
+              <b>ant design vue</b>
+              footer part
+            </div>
+          </template>
+          <template #renderItem="{ item }">
+            <a-list-item key="item.title">
+              <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+              </template>
+              <template #extra>
+                <img
+                    width="272"
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              </template>
+              <a-list-item-meta :description="item.description">
+                <template #title>
+                  <a :href="item.href">{{ item.title }}</a>
+                </template>
+                <template #avatar><a-avatar :src="item.avatar" /></template>
+              </a-list-item-meta>
+              {{ item.content }}
+            </a-list-item>
+          </template>
+        </a-list>
       </a-layout-content>
     </a-layout>
   </a-layout-content>
@@ -60,6 +88,20 @@
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
 import { defineComponent, ref ,onMounted ,reactive ,toRef} from 'vue';
 import axios from 'axios'
+
+const listData: Record<string, string>[] = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
   components: {
     UserOutlined,
@@ -70,6 +112,18 @@ export default defineComponent({
     //ref()可以定义响应式的数据
     const ebooks = ref()
     const ebooks1 = reactive({books:[]})
+
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
 
     //写到setup方法中，有可能导致界面还未渲染出来，操作DOM就会报错
     onMounted(()=>{
@@ -87,6 +141,9 @@ export default defineComponent({
       selectedKeys1: ref<string[]>(['2']),
       selectedKeys2: ref<string[]>(['1']),
       openKeys: ref<string[]>(['sub1']),
+      listData,
+      pagination,
+      actions
     };
   },
 });
